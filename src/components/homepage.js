d70a4ac55053b562e03cb274150158302b5cc798
Binish <bjnish8@gmail.com>
2020-02-02 04:32:19 -0500

    Added steps

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './styles/homepage.scss'
 import myvideo from '../Live-Music.mp4'
 import augventsLogo from '../augventsLogo.png'
+import { Icon } from 'antd'
 
 const HomePage = () => {
     return (
@@ -16,18 +17,35 @@ const HomePage = () => {
                     <button className="book-event"> book event </button>
                 </div>
             </div>
-
-            <div className="steps row">
-                <div>
-                    <p>01</p>
-                </div>
-                <div>
-                    <p> 02</p>
-                </div>
-                <div>
-                   <p> 03 </p>
+            <div className="steps">
+                <div className="title">
+                <p>4 steps to get started</p>
+                <hr />
+                    </div>
+                <div className="row">
+                    <div className="column">
+                        <p className="step">01</p>
+                        <Icon className="icon" type="mobile" />
+                        <span> sign up </span>
+                        <p className="plan-description"> Enter your name, email and phone number. </p>
+                    </div>
+                    <div className="column">
+                        <p className="step"> 02</p>
+                        <Icon className="icon" type="edit" />
+                        <span> pick plan </span>
+                        <p className="plan-description"> Select one of three plans. Your selected plan can
+                            be changed at a later date.
+                    </p>
+                    </div>
+                    <div className="column">
+                        <p className="step"> 03 </p>
+                        <Icon className="icon" type="check-square" />
+                        <span> book event </span>
+                        <p className="plan-description"> Provide the basic information about your event </p>
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }

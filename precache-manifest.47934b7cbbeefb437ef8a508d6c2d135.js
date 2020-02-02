759652546e085c0735fb97aeb1b787fe16ae8ba3
Binish <bjnish8@gmail.com>
2020-02-02 07:03:15 -0500

    Updates

---
+++ precache-manifest.47934b7cbbeefb437ef8a508d6c2d135.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90dc7653428cfe7e4d8bcb4c9e15bf60",
+    "url": "/augvents-react/index.html"
+  },
+  {
+    "revision": "cd7fd7eb8913f7cb65c3",
+    "url": "/augvents-react/static/css/2.8d6f6e05.chunk.css"
+  },
+  {
+    "revision": "c5859cdfd74b3e2119f5",
+    "url": "/augvents-react/static/css/main.ec1a43a5.chunk.css"
+  },
+  {
+    "revision": "cd7fd7eb8913f7cb65c3",
+    "url": "/augvents-react/static/js/2.de49e025.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/augvents-react/static/js/2.de49e025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5859cdfd74b3e2119f5",
+    "url": "/augvents-react/static/js/main.c3d843fa.chunk.js"
+  },
+  {
+    "revision": "54f1fea2d427096c2198",
+    "url": "/augvents-react/static/js/runtime-main.31b5b52c.js"
+  },
+  {
+    "revision": "cdf61c320c40f87430769218a1a9c5e7",
+    "url": "/augvents-react/static/media/Live-Music.cdf61c32.mp4"
+  },
+  {
+    "revision": "9539a81c00c1c194a6caf289d6cd3c5c",
+    "url": "/augvents-react/static/media/event.9539a81c.jpeg"
+  },
+  {
+    "revision": "e5f752d2c4188f3dc1a266cebf46f694",
+    "url": "/augvents-react/static/media/marble.e5f752d2.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/augvents-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/augvents-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/augvents-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/augvents-react/static/media/slick.f97e3bbf.svg"
+  }
+]);

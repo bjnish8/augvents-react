e12ae46dbc86d719a685865a3ab259c42f93e181
Binish <bjnish8@gmail.com>
2020-02-08 14:06:56 -0500

    Styled plans

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -6,9 +6,9 @@ import Intro from './intro.augvents'
 import AugVideo from './video'
 import Steps from './four.steps'
 import About from './about.augvents'
+import Plans from './plans.augvents'
 
 const HomePage = () => {
-
     const focusBorders = { border: "3px solid rgb(79, 181, 184)", borderRadius: "5px" }
     const [border, setBorder] = useState({})
     const highlightBlock = (index) => {
@@ -27,37 +27,7 @@ const HomePage = () => {
                 <Intro />
                 <Steps />
                 <About />
-                <div className="plans column">
-                    <div className="title">
-                        <p> 3 plans to choose from  </p>
-                        <hr />
-                    </div>
-                    <div className="row">
-                        <div className="column plan-block" style={border.block1} onClick={() => highlightBlock(1)}>
-                            <p className="step">pay as you go</p>
-                            <img src="https://cdn.stumagz.com/images/5739ae428cf7bstryimg"></img>
-                            <span> $8 / 1 Event </span>
-                            <p className="plan-description"> Easy and inexpensive way to get the word out about your pop up event. </p>
-                        </div>
-                        <div className="column plan-block" style={border.block2} onClick={() => highlightBlock(2)}>
-                            <p className="step"> premium</p>
-                            <img src="https://cdn.stumagz.com/images/5739ae428cf7bstryimg"></img>
-                            <span>  $65 / 10 Events  </span>
-                            <p className="plan-description"> Planning on hosting a series of musical or art event?
-                             This is the right plan for you!
-                    </p>
-                        </div>
-                        <div className="column plan-block" style={border.block3} onClick={() => highlightBlock(3)}>
-                            <p className="step"> enterprise </p>
-                            <img src="https://cdn.stumagz.com/images/5739ae428cf7bstryimg"></img>
-                            <span> $250 / 50 Events  </span>
-                            <p className="plan-description"> If booking acts many nights of each week is your responsiblity then you
-                        will want the simpolicity and low-cost of the enterprise plan. </p>
-                        </div>
-
-                    </div>
-                    <button className="book-event" style={{ width: "140px", height: "43px", margin: "70px 0px 0px" }}> next </button>
-                </div>
+                <Plans />
                 <div className="slider">
                     <ImageSlider />
                 </div>

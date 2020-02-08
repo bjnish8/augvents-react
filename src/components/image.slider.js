bd3ab54eb1bcdded58868dc95983f8f199a129d2
Binish <bjnish8@gmail.com>
2020-02-08 17:09:07 -0500

    updated image slider

--- src/components/image.slider.js
+++ src/components/image.slider.js
@@ -1,51 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 import augventsLogo from "../event.jpeg"
+import liveMusic from "../Live-Music.jpg"
 import './styles/image.slider.scss'
 
-export default class ImageSlider extends Component {
-  render() {
-    const settings = {
-      infinite: true,
-      speed: 3500,
-      autoplaySpeed: 1000,
-      slidesToShow: 3,
-      autoplay: true,
-      slidesToScroll: 1,
-      arrows: false,
-    };
-    return (
-      <div className="image-slider" >
-        <Slider {...settings}>
-          <div>
+const ImageSlider = (props) => {
+  const settings = {
+    infinite: true,
+    speed: 3500,
+    autoplaySpeed: 1000,
+    slidesToShow: 3,
+    autoplay: true,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+  return (
+    <div className="image-slider" >
+      <Slider {...settings}>
+        <div>
           <img src={augventsLogo}></img>
-          </div>
-          <div>
+        </div>
+        <div>
+          <img src={liveMusic}></img>
+        </div>
+        <div>
           <img src={augventsLogo}></img>
-          </div>
-          <div>
+        </div>
+        <div>
+          <img src={liveMusic}></img>
+        </div>
+        <div>
           <img src={augventsLogo}></img>
-          </div>
-          <div>
+        </div>
+        <div>
+          <img src={liveMusic}></img>
+        </div>
+        <div>
           <img src={augventsLogo}></img>
-          </div>
-          <div>
+        </div>
+        <div>
+          <img src={liveMusic}></img>
+        </div>
+        <div>
           <img src={augventsLogo}></img>
-          </div>
-          <div>
-          <img src={augventsLogo}></img>
-          </div>
-          <div>
-          <img src={augventsLogo}></img>
-          </div>
-          <div>
-          <img src={augventsLogo}></img>
-          </div>
-          <div>
-          <img src={augventsLogo}></img>
-          </div>
-        </Slider>
-      </div>
-    );
-  }
+        </div>
+      </Slider>
+    </div>
+  );
 }
+
+export default ImageSlider

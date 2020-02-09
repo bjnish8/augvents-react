beacd227599a7168678372b20d2de6698cc6fdd9
Binish <bjnish8@gmail.com>
2020-02-09 16:39:13 -0500

    Updated media queries

--- src/components/four.steps.js
+++ src/components/four.steps.js
@@ -11,34 +11,37 @@ const Steps = () => {
     return (
         <div className="steps">
             <Title {...{ params }} />
-            <div className="row">
-                <Step params={{
-                    index: "01",
-                    title: "sign up",
-                    icon: "mobile",
-                    content: "Enter your name, email and phone number. "
-                }} />
+            <div className="content">
+                <div className="level">
+                    <Step params={{
+                        index: "01",
+                        title: "sign up",
+                        icon: "mobile",
+                        content: "Enter your name, email and phone number. "
+                    }} />
 
-                <Step params={{
-                    index: "02",
-                    title: "pick plan",
-                    icon: "edit",
-                    content: "Select one of three plans. Your selected plan can be changed at a later date."
-                }} />
+                    <Step params={{
+                        index: "02",
+                        title: "pick plan",
+                        icon: "edit",
+                        content: "Select one of three plans. Your selected plan can be changed at a later date."
+                    }} />
+                </div>
+                <div className="level">
+                    <Step params={{
+                        index: "03",
+                        title: "book event",
+                        icon: "check-square",
+                        content: "Provide the basic information about your event."
+                    }} />
 
-                <Step params={{
-                    index: "03",
-                    title: "book event",
-                    icon: "check-square",
-                    content: "Provide the basic information about your event."
-                }} />
-
-                <Step params={{
-                    index: "04",
-                    title: "go live",
-                    icon: "check",
-                    content: "Once the information has been entered and submitted, the event is ready to go live. The AUGVENTS app will find it. "
-                }} />
+                    <Step params={{
+                        index: "04",
+                        title: "go live",
+                        icon: "check",
+                        content: "Once the information has been entered and submitted, the event is ready to go live. The AUGVENTS app will find it. "
+                    }} />
+                </div>
             </div>
         </div>
     );

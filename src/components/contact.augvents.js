faa4f71112e57b90461b72728f2f120be092a7b8
Binish <bjnish8@gmail.com>
2020-02-09 17:07:36 -0500

    Fixed styles for mobile

--- src/components/contact.augvents.js
+++ src/components/contact.augvents.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Input } from 'antd';
+import { Input, Icon } from 'antd';
 import Title from './minicomponents/augvents.title'
 import './styles/contact.augvents.scss'
 
-const Contact = () => { 
+const Contact = () => {
     const params = {
         titleText: "contact us"
     }
     return (
         <div className="contact">
             <Title {...{ params }} />
-            <span className="address">Address:  500 Lehman Ave.  Bowling Green, OH 43402-3089   | info@augvents.com </span>
+            <div className="address">
+                <div> <Icon className="icon" type="mail" /> info@augvents.com </div>
+                <div>  <Icon className="icon" type="compass" /> 500 Lehman Ave.  Bowling Green, OH 43402-3089 </div>
+            </div>
             <div className="column subscribe">
                 <span> Subscribe for updates and promotions </span>
                 <Input placeholder="Email Address"></Input>
